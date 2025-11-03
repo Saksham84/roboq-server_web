f5f4dbaacfae0da7a120ffb09e2dc9e339ac5dcb
Hello Coder ☠️ <sakshamjindal84@gmail.com>
2025-11-03 22:23:58 +0530

    connect railway database

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // Update if needed
-  database: 'roboq' // Your database name
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD, // Update if needed
+  database: process.env.DATABASE // Your database name
 });
 
 db.connect((err) => {
